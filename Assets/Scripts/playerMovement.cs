910f5e5f4069cfc3b94694b43236a2bea7edd18f
smahalgavaiya <36693350+smahalgavaiya@users.noreply.github.com>
2021-06-11 12:01:48 +0530

    created idle and walk animation from spirtsheet also added them and coded them to function with movement of player

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+
+	float verticalMove = 0f;
+
+	public Animator playerAnimator;
+	
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+		if (horizontalMove > 0 || verticalMove > 0)
+		{
+			playerAnimator.SetBool("IsWalking", true);
+		}
+		else
+		{
+			playerAnimator.SetBool("IsWalking", false);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, verticalMove * Time.fixedDeltaTime);
+	}
+}
